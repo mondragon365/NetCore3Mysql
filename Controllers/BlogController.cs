86c3d2cba3a42d0f61c2db0ac9005aeb3a0d0d3c
mondragon365 <lord_drako07@hotmail.com>
2019-11-26 22:09:25 -0500

    ok

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core3Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        public AppDb Db { get; }
+        public BlogController(AppDb db)
+        {
+            Db = db;
+        }
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Db.Connection.Open();
+            var query = new BlogPostQuery(Db);
+            var result = query.LatestPostsAsync();
+            return new OkObjectResult(result);
+        }
+    }
+}
